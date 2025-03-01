00226882635c31a518742cd37694899c0aa828da
im-namanjain <namanjainalwar12@gmail.com>
2025-03-02 02:04:11 +0530

    Working with http and dio

--- dart_understanding/lib/tiles.dart
+++ dart_understanding/lib/tiles.dart
@@ -1,5 +1,8 @@
 import 'package:dart_understanding/callingconstructor.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
+import 'dart:convert' as convert;
 
 class Tiles extends StatefulWidget {
   const Tiles({super.key});
@@ -9,9 +12,35 @@ class Tiles extends StatefulWidget {
 }
 
 class _TilesState extends State<Tiles> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  getHttpData() async {
+    http.Response response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+    // print(response.body);
+    if (response.statusCode == 200) {
+      List<dynamic> jsonResponse = convert.jsonDecode(response.body);
+      // var itemCount = jsonResponse['totalItems'];
+      print(jsonResponse[0]["title"]);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+
+  getDioData() async {
+    final dio = Dio();
+    final response =
+        await dio.get('https://jsonplaceholder.typicode.com/photos');
+    print(response.data[0]["title"]);
+  }
+
   ConstList list = ConstList();
   @override
   Widget build(BuildContext context) {
+    getDioData();
     return Scaffold(
       body: ListView.builder(
         itemCount: list.student.length,
