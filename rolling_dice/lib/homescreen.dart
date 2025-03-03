0239f4f5877970a1c0a2d93bc9b743603ec90384
im-namanjain <namanjainalwar12@gmail.com>
2025-03-04 00:08:58 +0530

    Rolling Dice Logic with setstate

--- rolling_dice/lib/homescreen.dart
+++ rolling_dice/lib/homescreen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,32 +10,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void roll() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Dicee"),
-      ),
+      appBar: AppBar(title: Text("Rolling Dice")),
       body: Center(
         child: Column(
           children: [
             Row(
               children: [
-                Expanded(child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Image(image: AssetImage('images/dice-png-1.png')),
-                )),
-                Expanded(child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Image(image: AssetImage('images/dice-png-1.png')),
-                ))
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Image(
+                      image: AssetImage('images/dice-png-$leftDiceNumber.png'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Image(
+                      image: AssetImage('images/dice-png-$rightDiceNumber.png'),
+                    ),
+                  ),
+                ),
               ],
             ),
-            ElevatedButton(onPressed: null,
-             child: Text(
-              "Roll",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),)
+            ElevatedButton(
+              onPressed: roll,
+              child: Text(
+                "Roll",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
           ],
         ),
       ),
